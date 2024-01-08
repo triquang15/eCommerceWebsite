4bb4aa490ec68c1eff3a2240048e3d2dfe1b0c6d
qnguyen95 <quangtri.nguyen@dxc.com>
2024-01-08 10:10:15 +0700

    #Code AmazonS3Util

--- WebParent/BackEnd/src/main/java/com/triquang/admin/BackEndApplication.java
+++ WebParent/BackEnd/src/main/java/com/triquang/admin/BackEndApplication.java
@@ -2,8 +2,10 @@ package com.triquang.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.triquang.common.entity"})
 public class BackEndApplication {
 
 	public static void main(String[] args) {
---
+++ WebParent/BackEnd/src/main/java/com/triquang/admin/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.triquang.admin;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.triquang.admin.paging.PagingAndSortingArgumentResolver;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new PagingAndSortingArgumentResolver());
+	}
+
+}
