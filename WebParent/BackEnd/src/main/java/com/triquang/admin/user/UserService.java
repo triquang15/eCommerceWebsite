0830c995afb3e29d8eaf3ebf8211c795e442a5bb
qnguyen95 <quangtri.nguyen@dxc.com>
2024-01-13 11:49:54 +0700

    #Show View Admin Page List User & User Form

--- WebParent/BackEnd/src/main/java/com/triquang/admin/user/UserService.java
+++ WebParent/BackEnd/src/main/java/com/triquang/admin/user/UserService.java
@@ -18,7 +18,7 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class UserService {
-	public static final int USERS_PER_PAGE = 4;
+	public static final int USERS_PER_PAGE = 5;
 	
 	@Autowired
 	private UserRepository userRepo;
